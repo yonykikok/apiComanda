7c63af0b721c7de7a8e331a33bcb081491224110
yonykikok <jonathan.n.haedo@gmail.com>
2019-11-29 20:31:59 -0300

    alta

--- src/Controllers/BartenderController.php
+++ src/Controllers/BartenderController.php
@@ -58,7 +58,6 @@ class BartenderController //implements IController
   public static function PrepararPedido($request,$response,$args)
   {
     $data=$request->getParsedBody();
-    var_dump($data);
     if(is_null($data))
     {
       $pedido=PedidoTrago::where('estado','pendiente')->get()->first();//obtengo el pedido que le sigue por orden
@@ -66,6 +65,7 @@ class BartenderController //implements IController
     }
     else
     {
+      var_dump($data);
       if(isset($data['orden']))// si ingresa una orden la busca y le da prioridad a esa orden
       {
         $pedido=PedidoTrago::where('estado','pendiente')->where('orden',$data['orden'])->get()->first();//obtengo el pedido que le sigue por orden
