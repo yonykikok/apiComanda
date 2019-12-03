3bdeaeaa4b0f52104089ff6bd3194371e71d92dd
yonykikok <jonathan.n.haedo@gmail.com>
2019-12-03 09:33:51 -0300

    alta

--- src/Routes/Cliente/ClienteRoutes.php
+++ src/Routes/Cliente/ClienteRoutes.php
@@ -9,9 +9,9 @@ use Middleware\RoleMiddleware;
 
 return function(App $app){
 $app->group('/Cliente/Pedido',function(){
-    $this->post('/',ClientesController::class.':VerPedido');
     $this->get('/verEstado',ClientesController::class.':verEstado');
     $this->get('/verDemora',ClientesController::class.':calcularDemora');
+    $this->post('/',ClientesController::class.':VerPedido');
     $this->post('/responderEncuesta',ClientesController::class.':GuardarEncuesta');
 });
 };
