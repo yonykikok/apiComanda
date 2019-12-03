935e252cfc554e489e0383a59969de086abf792a
yonykikok <jonathan.n.haedo@gmail.com>
2019-12-03 07:08:42 -0300

    alta

--- src/Routes/Cliente/ClienteRoutes.php
+++ src/Routes/Cliente/ClienteRoutes.php
@@ -12,7 +12,7 @@ $app->group('/Cliente/Pedido',function(){
     $this->get('/',ClientesController::class.':VerPedido');
     $this->get('/verEstado',ClientesController::class.':verEstado');
     $this->get('/verDemora',ClientesController::class.':calcularDemora');
-    $this->post('/{nombre}',ClientesController::class.':obtenerPedidosPorUsuario');
+    //$this->post('/{nombre}',ClientesController::class.':obtenerPedidosPorUsuario');
     $this->post('/responderEncuesta',ClientesController::class.':GuardarEncuesta');
 });
 };
