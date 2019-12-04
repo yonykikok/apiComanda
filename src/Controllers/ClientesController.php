6cfd6103bfc88d130cdfd95cffa9100d7c30d7ec
yonykikok <jonathan.n.haedo@gmail.com>
2019-12-04 17:03:03 -0300

    encuesta de satisfaccion

--- src/Controllers/ClientesController.php
+++ src/Controllers/ClientesController.php
@@ -196,7 +196,7 @@ class ClientesController //implements IController
   public static function GuardarEncuesta($request, $response, $args)
   {
     $datosDeLaEncuesta =  $request->getParsedBody();
-    var_dump($request);
+    var_dump($datosDeLaEncuesta);
     // if (self::EsUnaEncuestaValida($datosDeLaEncuesta)) {
     //   if (self::SonDatosValidos($datosDeLaEncuesta)) {
     //     $pedidoMozo = PedidoMozo::where('mesa', $datosDeLaEncuesta['mesa'])->where('orden', $datosDeLaEncuesta['orden'])->first();
